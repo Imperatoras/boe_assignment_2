c539e69d04913f95b2c504b35b9239b44f81d57b
Helena Williams <hwilliams10@stuy.edu>
2020-05-04 18:27:14 -0400

    fixed rt

--- isRightTriangle.js
+++ isRightTriangle.js
@@ -1,5 +1,14 @@
 const isRightTriangle = (a, b, c) => {
-  if(a*a + b*b == c*c){
+  if(a == 0 || b == 0){
+    return(false);
+  }
+  if(Math.pow(a,2) + Math.pow(b,2) == Math.pow(c,2)){
+    return(true);
+  }
+  if(Math.pow(a,2) + Math.pow(c,2) == Math.pow(b,2)){
+    return(true):
+  }
+  if(Math.pow(b,2) + Math.pow(c,2) == Math.pow(a,2)){
     return(true);
   }
   return(false);
