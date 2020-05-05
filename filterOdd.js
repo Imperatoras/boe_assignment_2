89cc5a3f6406d235e291ebf0c009d6e4b45ca1cb
Helena Williams <hwilliams10@stuy.edu>
2020-05-05 01:50:44 -0400

    maybe?

--- filterOdd.js
+++ filterOdd.js
@@ -1,7 +1,7 @@
 const filterOdd = (arr) => {
   let a = [];
-  for(i in arr){
-    if (i % 2 == 1) a.push(i);
+  for(int i = 0; i<arr.length; i++){
+    if (arr[i] % 2 == 1) a.push(arr[i]);
   }
   return(a);
 };
