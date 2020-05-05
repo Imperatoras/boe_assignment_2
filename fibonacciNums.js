67c3c806efa4069d2754641ba6e0b270700893ca
Helena Williams <hwilliams10@stuy.edu>
2020-05-05 01:18:35 -0400

    HIGHLY EXPERIMENTAL

--- fibonacciNums.js
+++ fibonacciNums.js
@@ -1,7 +1,13 @@
 const fibonacciNums = (n) => {
   let result = [];
   if(n <= 0) return(result);
-  return([1,2,3]);
+  let result = [1];
+  while(result.push(5)<=n){
+    if(result.length() == 1) result.push(1);
+    else{
+      result.push(result[result.length()-1] + result[result.length()-2]);
+    }
+  }
 };
 
 module.exports = fibonacciNums;
