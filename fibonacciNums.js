b54f96b84a3cc91ca6f1c7a7650c4b5b0edaef09
Helena Williams <hwilliams10@stuy.edu>
2020-05-05 01:23:18 -0400

    back to the drawing board

--- fibonacciNums.js
+++ fibonacciNums.js
@@ -2,13 +2,7 @@ const fibonacciNums = (n) => {
   let result = [];
   if(n <= 0) return(result);
   result.push(1);
-  while(result.push(5)<=n){
-    if(result.length == 1) result.push(1);
-    else{
-      result.push(2);
-    //  result.push(result[result.length-1] + result[result.length-2]);
-    }
-  }
+  return(result);
 };
 
 module.exports = fibonacciNums;
