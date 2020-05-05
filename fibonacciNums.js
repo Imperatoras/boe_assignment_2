bcff4260ebbbe27f54272cdec461d5d9c4e50a09
Helena Williams <hwilliams10@stuy.edu>
2020-05-05 01:29:10 -0400

    listening to someone screaming the bible helps

--- fibonacciNums.js
+++ fibonacciNums.js
@@ -2,7 +2,7 @@ const fibonacciNums = (n) => {
   let result = [];
   if(n <= 0) return(result);
   result.push(0);
-  while(result.push(5)<=n){
+  while(result.length<=n){
     if(result.length == 1) result.push(1);
     else{
       result.push(result[result.length-1] + result[result.length-2]);
