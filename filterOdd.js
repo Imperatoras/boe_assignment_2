6705018e1a12fe7cb6282d8ed1dc5d9ee7d08a36
Helena Williams <hwilliams10@stuy.edu>
2020-05-05 01:53:23 -0400

    back to basics

--- filterOdd.js
+++ filterOdd.js
@@ -1,9 +1,7 @@
 const filterOdd = (arr) => {
   let a = [];
   for(int i = 0; i<arr.length; i++){
-    if (arr[i]%2 == 1){
-      a.push(arr[i]);
-    }
+    a.push(arr[i]);
   }
   return(a);
 };
--- getFrequencies.js
+++ getFrequencies.js
@@ -3,11 +3,17 @@ const getFrequencies = (arr) => {
   ret.nums = [];
   ret.freq = [];
   for(i in arr){
-    if (ret.nums.includes(i)) ret.nums[ret.nums.indexOf(i)]++;
-    else ret.nums.push(i);
+    if (ret.nums.includes(i)){
+      ret.nums[ret.nums.indexOf(i)]++;
+    }
+    else {
+      ret.nums.push(i);
+    }
   }
   return(ret);
 
 };
 
 module.exports = getFrequencies;
+
+//FUCK IT WE'LL COME BACK TO THIS IN A BIT BECAUSE NOOO.
