4e94af63a0dca9c67807d44bfa9508e32674870e
Helena Williams <hwilliams10@stuy.edu>
2020-05-05 01:52:41 -0400

    confuzzled

--- filterOdd.js
+++ filterOdd.js
@@ -1,7 +1,9 @@
 const filterOdd = (arr) => {
   let a = [];
   for(int i = 0; i<arr.length; i++){
-    if (arr[i] % 2 == 1) a.push(arr[i]);
+    if (arr[i]%2 == 1){
+      a.push(arr[i]);
+    }
   }
   return(a);
 };
