459e6a89197772f0f92bd8e58811bbf429434c04
Helena Williams <hwilliams10@stuy.edu>
2020-05-06 01:23:23 -0400

    very small

--- getFrequencies.js
+++ getFrequencies.js
@@ -2,7 +2,7 @@ const getFrequencies = (arr) => {
   var dict = new Object();
   var str = "";
   for(int i = 0; i < arr.length; i++){
-    dict[i] = arr[i];
+    dict[i] = 1;
   }
   return(dict);
 };
