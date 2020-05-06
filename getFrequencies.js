273f4a8f4e134e56ea8ad3309d55031f752220d9
Helena Williams <hwilliams10@stuy.edu>
2020-05-06 01:18:44 -0400

    back to basics

--- getFrequencies.js
+++ getFrequencies.js
@@ -1,9 +1,9 @@
 const getFrequencies = (arr) => {
   var dict = new Object();
   for(int i = 0; i < arr.length; i++){
-    var str = arr[i];
-    if(dict[str] == "undefined") dict[str] = 1;
-    else dict[str] = dict[str]++;
+    //var str = arr[i];
+    //if(dict[str] == "undefined") dict[str] = 1;
+    //else dict[str] = dict[str]++;
   }
   return(dict);
 };
