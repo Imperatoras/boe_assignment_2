d19990ec2016b05c822c264228c4f1263ccb9462
Helena Williams <hwilliams10@stuy.edu>
2020-05-06 01:16:56 -0400

    str test

--- getFrequencies.js
+++ getFrequencies.js
@@ -1,8 +1,9 @@
 const getFrequencies = (arr) => {
   var dict = new Object();
   for(int i = 0; i < arr.length; i++){
-    if(dict[arr[i]] == "undefined") dict[arr[i]] = 1;
-    else dict[arr[i]] = dict[arr[i]]++;
+    let str = arr[i];
+    if(dict[str] == "undefined") dict[str] = 1;
+    else dict[str] = dict[str]++;
   }
   return(dict);
 };
