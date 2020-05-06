26c73224adf1642b9236f2e6f0be71642350bf9d
Helena Williams <hwilliams10@stuy.edu>
2020-05-06 01:15:28 -0400

    removed for in

--- getFrequencies.js
+++ getFrequencies.js
@@ -1,6 +1,6 @@
 const getFrequencies = (arr) => {
   var dict = new Object();
-  for(i in arr){
+  for(int i = 0; i < arr.length; i++){
     if(dict[arr[i]] == "undefined") dict[arr[i]] = 1;
     else dict[arr[i]] = dict[arr[i]]++;
   }
