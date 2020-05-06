b5a209b1f251edba4ac731fff8c5ac144771aed0
Helena Williams <hwilliams10@stuy.edu>
2020-05-06 01:20:03 -0400

    okay really basic

--- getFrequencies.js
+++ getFrequencies.js
@@ -1,11 +1,6 @@
 const getFrequencies = (arr) => {
   var dict = new Object();
-  for(int i = 0; i < arr.length; i++){
-    //var str = arr[i];
-    //if(dict[str] == "undefined") dict[str] = 1;
-    //else dict[str] = dict[str]++;
-  }
-  return(arr);
+  return(dict);
 };
 
 module.exports = getFrequencies;
