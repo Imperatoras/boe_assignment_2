a4a78662df5e7ac22dda6beb0d19f6464315f3f2
Helena Williams <hwilliams10@stuy.edu>
2020-05-06 01:29:15 -0400

    Simple dict test

--- getFrequencies.js
+++ getFrequencies.js
@@ -2,8 +2,7 @@ const getFrequencies = (arr) => {
   var dict = new Object();
   var str = "";
   for(var i = 0; i < arr.length; i++){
-    if(dict[arr[i]]!="undefined") dict[arr[i]] = 1;
-    else dict[arr[i]] = dict[arr[i]]++;
+    dict[i] = arr[i];
   return(dict);
 };
 
