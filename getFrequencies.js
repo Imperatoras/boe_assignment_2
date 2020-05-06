b962d47ebb66a98da6aa3e07c30797b08149d920
Helena Williams <hwilliams10@stuy.edu>
2020-05-06 01:31:15 -0400

    workaround?

--- getFrequencies.js
+++ getFrequencies.js
@@ -2,7 +2,11 @@ const getFrequencies = (arr) => {
   var dict = new Object();
   var str = "";
   for(var i = 0; i < arr.length; i++){
-    dict[i] = arr[i];
+    dict[arr[i]] = 0;
+  }
+  for(var a = 0; a < arr.length; a++){
+    dict[arr[a]] = dict[arr[a]]++;
+  }
   return(dict);
 };
 
