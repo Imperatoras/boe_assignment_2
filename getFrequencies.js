336a0260356df7e6dee15fbfb2921377fcc98b59
Helena Williams <hwilliams10@stuy.edu>
2020-05-06 01:17:57 -0400

    var fix

--- getFrequencies.js
+++ getFrequencies.js
@@ -1,7 +1,7 @@
 const getFrequencies = (arr) => {
   var dict = new Object();
   for(int i = 0; i < arr.length; i++){
-    let str = arr[i];
+    var str = arr[i];
     if(dict[str] == "undefined") dict[str] = 1;
     else dict[str] = dict[str]++;
   }
