76bd3f1a0d0e307bdfa21bf785db7c25a08df662
Helena Williams <hwilliams10@stuy.edu>
2020-05-06 01:46:26 -0400

    fixer upper

--- getFrequencies.js
+++ getFrequencies.js
@@ -1,15 +1,12 @@
 const getFrequencies = (arr) => {
   var dict = new Object();
-  var str = "";
   for(var i = 0; i < arr.length; i++){
     dict[arr[i]] = 0;
   }
   for(var a = 0; a < arr.length; a++){
-    dict[arr[a]] = dict[arr[a]]++;
+    dict[arr[a]] = ++dict[arr[a]];
   }
   return(dict);
 };
 
 module.exports = getFrequencies;
-
-//FUCK IT WE'LL COME BACK TO THIS IN A BIT BECAUSE NOOO.
